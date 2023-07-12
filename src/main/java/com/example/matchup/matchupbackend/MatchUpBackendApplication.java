9ab65499024e3f17b47d0388e097e481835d214e
JunHyuk <ericyoo0107@gmail.com>
2023-07-12 22:44:24 +0900

    initial commit

---
+++ src/main/java/com/example/matchup/matchupbackend/MatchUpBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.matchup.matchupbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchUpBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MatchUpBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/matchup/matchupbackend/MatchUpBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.matchup.matchupbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchUpBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
