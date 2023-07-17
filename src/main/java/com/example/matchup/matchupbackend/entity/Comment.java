d5629bb919609c1b22b57a6fa3c565070b4f3d85
JunHyuk <ericyoo0107@gmail.com>
2023-07-17 23:34:39 +0900

    feat: 엔티티 추가(1차)

---
+++ src/main/java/com/example/matchup/matchupbackend/entity/Comment.java
@@ -0,0 +1,29 @@
+package com.example.matchup.matchupbackend.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Entity
+@Getter
+@Table(name = "comment")
+public class Comment {
+
+    @Id
+    @Column(name = "comment_id")
+    private Long id;
+
+    @Column(name = "comment_content")
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "links_id")
+    private Links links;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "feed_id")
+    private Feed feed;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+}
