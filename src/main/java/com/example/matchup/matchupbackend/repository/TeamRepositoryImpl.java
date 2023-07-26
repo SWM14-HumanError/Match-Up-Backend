f493a42f5a6b707361f2512159c7e8d1d2fd4b0d
JunHyuk <ericyoo0107@gmail.com>
2023-07-26 15:33:08 +0900

    refactor: predicate -> BooleanExpression

--- src/main/java/com/example/matchup/matchupbackend/repository/TeamRepositoryImpl.java
+++ src/main/java/com/example/matchup/matchupbackend/repository/TeamRepositoryImpl.java
@@ -3,8 +3,6 @@ package com.example.matchup.matchupbackend.repository;
 import com.example.matchup.matchupbackend.dto.TeamSearchRequest;
 import com.example.matchup.matchupbackend.dto.TeamSearchResponse;
 import com.example.matchup.matchupbackend.entity.QTeam;
-import com.example.matchup.matchupbackend.entity.Team;
-import com.querydsl.core.types.Predicate;
 import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
@@ -12,11 +10,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.domain.SliceImpl;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 import static com.example.matchup.matchupbackend.entity.QTeam.*;
-
+@Repository
 @RequiredArgsConstructor
 public class TeamRepositoryImpl implements TeamRepositoryCustom{
     private final JPAQueryFactory queryFactory;
@@ -51,18 +50,18 @@ public class TeamRepositoryImpl implements TeamRepositoryCustom{
         return new SliceImpl<>(content, pageable, hasNext);
     }
 
-    private Predicate searchEq(String search) {
+    private BooleanExpression searchEq(String search) {
         return (search != null) ? team.title.contains(search) : null;
     }
 // tag 중 하나라도 있으면 가져옴
-    private Predicate categoryEq(String category) {
+    private BooleanExpression categoryEq(String category) {
         if(category != null){ //태그 안에 카테고리가 있는지 찾아야함
             return team.tags.any().name.eq(category); // any하면 리스트를 통째로 들고 온다
         }
         else return null;
     }
 
-    private Predicate typeEq(Long type) {
+    private BooleanExpression typeEq(Long type) {
         return (type != null) ? team.type.eq(type) : null;
     }
 
