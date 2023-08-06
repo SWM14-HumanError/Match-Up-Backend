fec26f01e096f8816758b958a1d71795ef1bcbc1
JunHyuk <ericyoo0107@gmail.com>
2023-08-07 01:08:56 +0900

    refactor: 정렬 기준을 long에서 string이 더 구분하기 쉬울것 같아 변경

--- src/main/java/com/example/matchup/matchupbackend/dto/user/UserSearchRequest.java
+++ src/main/java/com/example/matchup/matchupbackend/dto/user/UserSearchRequest.java
@@ -8,7 +8,7 @@ public class UserSearchRequest {
     private String place; //사는곳, 만남 위치
     private String techStack; //가지고 있는 기술 스택
     private String position; // 하고 싶은 포지션
-    private Long orderBy = 0L; //정렬 기준
+    private String orderBy = "default"; //정렬 기준
     private int page;
     private int size;
 }
