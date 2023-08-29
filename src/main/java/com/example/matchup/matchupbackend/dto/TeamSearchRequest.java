bef78fed69b1d56911dfeb1ff42fe7933d072703
JunHyuk <ericyoo0107@gmail.com>
2023-08-29 16:48:56 +0900

    feat: 메인페이지 보여줄때 request에 메세지 추가

--- src/main/java/com/example/matchup/matchupbackend/dto/TeamSearchRequest.java
+++ src/main/java/com/example/matchup/matchupbackend/dto/TeamSearchRequest.java
@@ -2,17 +2,16 @@ package com.example.matchup.matchupbackend.dto;
 
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
-import lombok.NonNull;
 import org.hibernate.validator.constraints.Range;
 
 @Data
 public class TeamSearchRequest {
-    @Range(max = 1L)
+    @Range(max = 1L, message = "0, 1 외에 다른 값이 오면 안됩니다 (0 = 프로젝트, 1 = 스터디)")
     private Long type;
     private String category;
     private String search;
-    @NotNull
+    @NotNull(message = "몇번째 페이지인지 입력하세요")
     private int page;
-    @NotNull
+    @NotNull(message = "한 페이지에 몇개씩 보여줄건지 정하세요")
     private int size;
 }
