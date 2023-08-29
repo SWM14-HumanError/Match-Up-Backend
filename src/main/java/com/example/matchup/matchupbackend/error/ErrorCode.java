45750fd0337cf73ff86dc37617ca9cd4da6296c7
JunHyuk <ericyoo0107@gmail.com>
2023-08-29 19:43:56 +0900

    feat: "@QueryParam으로 request 왔는데 빈 검증(@Valid)에 통과 못한 경우" 글로벌 예외 처리

--- src/main/java/com/example/matchup/matchupbackend/error/ErrorCode.java
+++ src/main/java/com/example/matchup/matchupbackend/error/ErrorCode.java
@@ -2,11 +2,12 @@ package com.example.matchup.matchupbackend.error;
 
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
+
 @Getter
 public enum ErrorCode {
-    SEVER_GLOBAL_ERROR(HttpStatus.INTERNAL_SERVER_ERROR , "GlobalError-001", "서버에 알 수 없는 오류가 발생했습니다.");
-
-
+    SEVER_GLOBAL_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "G-001", "서버에 알 수 없는 오류가 발생했습니다."),
+    INVALID_REQUEST_ENCODE(HttpStatus.BAD_REQUEST, "G-002", "JSON 형식 요청이 아닙니다."),
+    REQUEST_FIELD_ERROR(HttpStatus.BAD_REQUEST, "G-003", "입력 필드에 오류가 있습니다.");
 
     private HttpStatus status;
     private String code;
--- src/main/java/com/example/matchup/matchupbackend/error/advice/GlobalAdvice.java
+++ src/main/java/com/example/matchup/matchupbackend/error/advice/GlobalAdvice.java
@@ -5,16 +5,48 @@ import com.example.matchup.matchupbackend.error.ErrorResult;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @RestControllerAdvice
 public class GlobalAdvice {
+    //--Natural Error--//
+    /**
+     * DTO의 request(@QueryParam)가 컨트롤러 @Valid에서 빈 검증 통과 못한 경우
+     */
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity MethodArgumentNotValidExHandler(MethodArgumentNotValidException ex) {
+        Map<String, List<String>> messageExtra = new HashMap<>(); //ex) type: [에러1, 에러2, 에러3...]
+        convertBindingResultToMap(ex, messageExtra);
+        ErrorResult errorResponseDto = ErrorResult.of(ErrorCode.REQUEST_FIELD_ERROR, messageExtra);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponseDto);
+    }
+
+    /**
+     * 그 외 서버 내부 오류인 경우
+     */
     @ExceptionHandler(Exception.class)
     public ResponseEntity globalExHandler(Exception ex) {
         log.error("[exceptionHandle] ex", ex);
         ErrorResult errorResult = ErrorResult.of(ErrorCode.SEVER_GLOBAL_ERROR);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResult);
+    }
+    //--Custom Error--//
+    //--매서드 모음--//
+    private void convertBindingResultToMap(BindingResult ex, Map<String, List<String>> messageExtra) {
+        if (ex.hasErrors()) {
+            ex.getFieldErrors()
+                    .forEach(fieldError ->
+                            messageExtra.computeIfAbsent(fieldError.getField(), key -> new ArrayList<>())
+                                    .add(fieldError.getDefaultMessage()));
+        }
     }
 }
