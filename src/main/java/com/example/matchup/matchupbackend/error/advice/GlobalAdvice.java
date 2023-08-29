9c2fd49aa42ad38526adf279a5870876512f9527
JunHyuk <ericyoo0107@gmail.com>
2023-08-30 00:36:02 +0900

    feat: 인가가 필요한 리소스에 토큰이 없는 유저가 접근 할때 예외 발생

--- src/main/java/com/example/matchup/matchupbackend/error/advice/GlobalAdvice.java
+++ src/main/java/com/example/matchup/matchupbackend/error/advice/GlobalAdvice.java
@@ -4,10 +4,10 @@ import com.example.matchup.matchupbackend.error.ErrorCode;
 import com.example.matchup.matchupbackend.error.ErrorResult;
 import com.example.matchup.matchupbackend.error.exception.AuthorizeException;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingRequestHeaderException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -22,6 +22,7 @@ import static org.springframework.http.HttpStatus.*;
 @RestControllerAdvice
 public class GlobalAdvice {
     //------------Natural Error------------//
+
     /**
      * DTO의 request(@QueryParam)가 컨트롤러 @Valid에서 빈 검증 통과 못한 경우  (G-003)
      */
@@ -34,20 +35,27 @@ public class GlobalAdvice {
     }
 
     /**
-     * 그 외 서버 내부 오류인 경우 (G-001)
+     * 토큰 없이 인가가 필요한 리소스에 유저가 접근하는 경우 (G-005)
      */
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity globalExHandler(Exception ex) {
-        log.error("[exceptionHandle] ex", ex);
-        ErrorResult errorResult = ErrorResult.of(ErrorCode.SEVER_GLOBAL_ERROR);
-        return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(errorResult);
+    @ExceptionHandler(MissingRequestHeaderException.class)
+    public ResponseEntity MissingRequestHeaderExHandler(MissingRequestHeaderException ex) {
+        ErrorResult errorResponseDto = ErrorResult.of(ErrorCode.MISSING_REQUEST_HEADER, ex.getHeaderName());
+        return ResponseEntity.status(BAD_REQUEST).body(errorResponseDto);
     }
 
+    /**
+     * 그 외 서버 내부 오류인 경우 (G-001)
 
+     @ExceptionHandler(Exception.class) public ResponseEntity globalExHandler(Exception ex) {
+     log.error("서버 오류 500");
+     ErrorResult errorResult = ErrorResult.of(ErrorCode.SEVER_GLOBAL_ERROR);
+     return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(errorResult);
+     }*/
 
     //------------Custom Error------------//
+
     /**
-     * 권한이 없는 유저가 접근하는 경우 (G-004)
+     * 잘못된 토큰을 가지고 있는 유저가 인가가 필요한 리소스에 접근하는 경우 (G-004)
      */
     @ExceptionHandler(AuthorizeException.class)
     public ResponseEntity AuthorizeExHandler(AuthorizeException ex) {
