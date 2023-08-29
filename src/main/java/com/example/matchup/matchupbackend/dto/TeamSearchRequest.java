cf9ff2bd7fe5d893e6dec32c76d4512567765552
JunHyuk <ericyoo0107@gmail.com>
2023-08-29 19:44:54 +0900

    feat: "메인페이지 API"에서 request DTO에 빈 검증 추가

--- src/main/java/com/example/matchup/matchupbackend/dto/TeamSearchRequest.java
+++ src/main/java/com/example/matchup/matchupbackend/dto/TeamSearchRequest.java
@@ -6,12 +6,11 @@ import org.hibernate.validator.constraints.Range;
 
 @Data
 public class TeamSearchRequest {
+    @NotNull(message = "null이 반환 되었습니다 -> 프로젝트 = 0, 스터디 = 1 둘중 하나를 선택하세요")
     @Range(max = 1L, message = "0, 1 외에 다른 값이 오면 안됩니다 (0 = 프로젝트, 1 = 스터디)")
     private Long type;
     private String category;
     private String search;
-    @NotNull(message = "몇번째 페이지인지 입력하세요")
     private int page;
-    @NotNull(message = "한 페이지에 몇개씩 보여줄건지 정하세요")
     private int size;
 }
