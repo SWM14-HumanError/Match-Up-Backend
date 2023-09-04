3a42484280e489067b1cee326be47c384ee44eee
jujemu <jujemu30@gmail.com>
2023-09-05 01:29:50 +0900

    Feat: Find slice of feed by search requestDto
    Edit
      - move static/imgs to s3 bucket
      - add thumnailUrl in team repository
    
    Add
      - /api/v1/list/feed, feed slice
      - secret code in gitignore
    
    Ref: #20 Issue

--- src/main/java/com/example/matchup/matchupbackend/entity/Feed.java
+++ src/main/java/com/example/matchup/matchupbackend/entity/Feed.java
@@ -4,7 +4,7 @@ import jakarta.persistence.*;
 import lombok.Getter;
 @Entity
 @Getter
-public class Feed {
+public class Feed extends BaseTimeEntity{
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "feed_id")
     private Long id;
@@ -16,6 +16,7 @@ public class Feed {
     private String content;
 
     private String thumbnailUrl;
+    private Long likeCount; // 아직 미구현(디자인 상에 구현되어 있지 않음)
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
