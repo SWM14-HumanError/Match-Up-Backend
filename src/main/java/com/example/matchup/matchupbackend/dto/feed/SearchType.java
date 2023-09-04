dd0201578d77dfb35e5b25c8f29a98bff2d1a00b
jujemu <jujemu30@gmail.com>
2023-09-05 01:29:50 +0900

    Feat: Find slice of feed by search requestDto
    Edit
      - move static/imgs to s3 bucket
      - add thumnailUrl in team repository
    
    Add
      - /api/v1/list/feed, feed slice
      - secret code in gitignore
    
    Ref: #20 Issue

---
+++ src/main/java/com/example/matchup/matchupbackend/dto/feed/SearchType.java
@@ -0,0 +1,5 @@
+package com.example.matchup.matchupbackend.dto.feed;
+
+public enum SearchType {
+    WRITER, TITLE
+}
--- src/main/java/com/example/matchup/matchupbackend/repository/feed/FeedRepository.java
+++ src/main/java/com/example/matchup/matchupbackend/repository/feed/FeedRepository.java
@@ -2,12 +2,11 @@ package com.example.matchup.matchupbackend.repository.feed;
 
 import com.example.matchup.matchupbackend.entity.Feed;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
-import java.util.List;
+public interface FeedRepository extends JpaRepository<Feed, Long>{
 
-public interface FeedRepository extends JpaRepository<Feed, Long> {
-
-    @Query("SELECT feed from Feed feed")
-    List<Feed> getFeedList();
+//    @Query(value = "select f from Feed f where f.user.name like %:search%")
+//    Slice<Feed> findFeedSliceBySearchRequest(@Param("search") String search,
+//                                             @Param("searchType") String searchType,
+//                                             Pageable pageable);
 }
