7cdd0723549c2f12d1669ba47d77b9e19bf81e7e
JunHyuk <ericyoo0107@gmail.com>
2023-09-12 19:34:16 +0900

    refactor: 주석 제거

--- src/main/java/com/example/matchup/matchupbackend/error/ErrorCode.java
+++ src/main/java/com/example/matchup/matchupbackend/error/ErrorCode.java
@@ -26,8 +26,8 @@ public enum ErrorCode {
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "U-S-001", "유저 정보가 존재하지 않습니다."),
     FILE_UPLOAD_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "F-S-001", "파일을 저장 중 오류가 발생했습니다."),
     FILE_EXTENSION_ERROR(HttpStatus.BAD_REQUEST, "F-S-002", "지원하지 않는 파일 확장자 입니다."),
-    MAX_FILE_SIZE_ERROR(HttpStatus.BAD_REQUEST, "F-S-003", "너무 큰 파일을 업로드 하였습니다.");
-    // FEED_NOT_FOUND(HttpStatus.NOT_FOUND, "FE-S-001", "피드 정보가 존재하지 않습니다."),
+    MAX_FILE_SIZE_ERROR(HttpStatus.BAD_REQUEST, "F-S-003", "너무 큰 파일을 업로드 하였습니다."),
+    FEED_NOT_FOUND(HttpStatus.NOT_FOUND, "FE-S-001", "피드 정보가 존재하지 않습니다.");
 
     private HttpStatus status;
     private String code;
