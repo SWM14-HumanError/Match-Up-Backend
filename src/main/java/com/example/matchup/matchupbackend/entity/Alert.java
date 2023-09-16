e59b1f2aa84004c3c6caa847be7dfe2837de08cf
Junhyuk Yu / 유준혁 <ericyoo0107@gmail.com>
2023-09-16 20:58:11 +0900

    feat: 알림을 저장 할 db에 column 구체적으로 추가

--- src/main/java/com/example/matchup/matchupbackend/entity/Alert.java
+++ src/main/java/com/example/matchup/matchupbackend/entity/Alert.java
@@ -6,19 +6,24 @@ import lombok.Getter;
 @Entity
 @Getter
 @Table(name = "alert")
-public class Alert {
-
+public class Alert extends BaseTimeEntity {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "alert_id")
     private Long id;
-
+    @Column(name = "title")
+    private String title;
     @Column(name = "content")
     private String content;
-
-    @Column(name = "alert_type")
-    private Integer alertType;
-
-    @ManyToOne(fetch = FetchType.LAZY)
+    @Column(name="is_read", columnDefinition = "boolean default false")
+    private Boolean isRead;
+    @Column(name = "is_deleted", columnDefinition = "boolean default false")
+    private Boolean isDeleted;
+    @Column(name = "redirect_url")
+    private String redirectUrl;
+    @Enumerated(EnumType.STRING)
+    private AlertType alertType;
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "user_id")
     private User user;
 }
+// 알림의 경우에 BaseTimeEntity에서 생성시간은 알림 생성 시간, 수정시간은 알림을 읽은 시간으로 설정하였습니다.
