7b2cdaa9c339643a72990f09eaa887e1d46d8b7f
Junhyuk Yu / 유준혁 <ericyoo0107@gmail.com>
2023-09-18 19:08:44 +0900

    feat: 알림 읽음 처리 API DAO 제작

--- src/main/java/com/example/matchup/matchupbackend/repository/alert/AlertRepository.java
+++ src/main/java/com/example/matchup/matchupbackend/repository/alert/AlertRepository.java
@@ -3,5 +3,8 @@ package com.example.matchup.matchupbackend.repository.alert;
 import com.example.matchup.matchupbackend.entity.Alert;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AlertRepository extends JpaRepository<Alert, Long>, AlertRepositoryCustom {
+    Optional<Alert> findByIdAndUserId(Long alertId, Long userId);
 }
