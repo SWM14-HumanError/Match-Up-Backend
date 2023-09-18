f6b916b70ceec7a895d6ffe3ebe5566a75a4a7a1
Junhyuk Yu / 유준혁 <ericyoo0107@gmail.com>
2023-09-18 23:46:41 +0900

    feat: 팀에 좋아요 누른 경우 피드 팀원들에게 좋아요 알림 전달

--- src/main/java/com/example/matchup/matchupbackend/service/AlertCreateService.java
+++ src/main/java/com/example/matchup/matchupbackend/service/AlertCreateService.java
@@ -222,7 +222,7 @@ public class AlertCreateService {
      * @param feed
      * @param likes
      */
-    public void saveFeedLikeAlert(User liker, Feed feed, Integer likes) {
+    public void saveFeedLikeAlert(User liker, Feed feed, Integer likes) { //todo 유저 많아지면 알림을 한번에 몰아서 보내는 방법도 생각해야 함
         Alert alert = Alert.builder()
                 .title(liker.getName() + " 님이 " + feed.getTitle() + " 에 좋아요를 눌렀습니다.")
                 .content("누적 좋아요 갯수 - " + likes)
@@ -234,6 +234,22 @@ public class AlertCreateService {
     }
 
     /**
+     * 팀에 좋아요를 눌렀을때 알림 생성
+     * @param liker
+     * @param team
+     * @param likes
+     */
+    public void saveTeamLikeAlert(User liker, Team team, Integer likes) { //todo 유저 많아지면 알림을 한번에 몰아서 보내는 방법도 생각해야 함
+        Alert alert = Alert.builder()
+                .title(liker.getName() + " 님이 " + team.getTitle() + " 에 좋아요를 눌렀습니다.")
+                .content("누적 좋아요 갯수 - " + likes)
+                .redirectUrl(team.getType() == 0L ? "/project/" + team.getId() : "/study/" + team.getId())
+                .alertType(team.getType() == 0L ? AlertType.PROJECT : AlertType.STUDY)
+                .build();
+        sendAlertToTeamUsers(team.getTeamUserList(), alert);
+    }
+
+    /**
      * 여러명의 유저에게 "같은 알림"을 보내는 메서드
      * @param sendTo
      * @param alert
@@ -242,6 +258,19 @@ public class AlertCreateService {
         for (User user : sendTo) {
             Alert newAlert = Alert.from(alert);
             newAlert.setUser(user);
+            alertRepository.save(newAlert);
+        }
+    }
+
+    /**
+     * 여러명의 "팀원"에게 "같은 알림"을 보내는 메서드
+     * @param sendTo
+     * @param alert
+     */
+    public void sendAlertToTeamUsers(List<TeamUser> sendTo, Alert alert) {
+        for (TeamUser teamUser : sendTo) {
+            Alert newAlert = Alert.from(alert);
+            newAlert.setUser(teamUser.getUser());
             alertRepository.save(newAlert);
         }
     }
