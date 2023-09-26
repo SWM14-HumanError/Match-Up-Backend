e183e98dee54345ed83725ef72500897ddc315be
JunHyuk Yu <ericyoo0107@gmail.com>
2023-09-26 13:28:53 +0900

    feat: 지원 정보 가져오는 DAO 추가

--- src/main/java/com/example/matchup/matchupbackend/repository/TeamRecruitRepository.java
+++ src/main/java/com/example/matchup/matchupbackend/repository/TeamRecruitRepository.java
@@ -2,6 +2,12 @@ package com.example.matchup.matchupbackend.repository;
 
 import com.example.matchup.matchupbackend.entity.TeamRecruit;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface TeamRecruitRepository  extends JpaRepository<TeamRecruit, Long> {
+import java.util.Optional;
+
+public interface TeamRecruitRepository extends JpaRepository<TeamRecruit, Long> {
+    @Query("select teamrecruit from TeamRecruit teamrecruit join fetch teamrecruit.user where teamrecruit.id = :recruitId")
+    Optional<TeamRecruit> findRecruitJoinUserById(@Param("recruitId") Long recruitId);
 }
