4e4289c9d5d5785d30714b2bb95b0a2887ba59b1
JunHyuk Yu <ericyoo0107@gmail.com>
2023-09-27 02:48:26 +0900

    feat: 유저 거절 폼 변경

--- src/main/java/com/example/matchup/matchupbackend/dto/request/teamuser/RefuseFormRequest.java
+++ src/main/java/com/example/matchup/matchupbackend/dto/request/teamuser/RefuseFormRequest.java
@@ -8,8 +8,6 @@ import lombok.Data;
 public class RefuseFormRequest {
     @NotNull(message = "팀에 지원했던 유저 ID는 필수 값입니다.")
     private Long recruitUserID;
-    @NotBlank(message = "팀에 지원했던 역할 입력은 필수 입니다")
-    private String role;
     @NotBlank(message = "거절 사유 입력은 필수 입니다")
     private String refuseReason;
 }
