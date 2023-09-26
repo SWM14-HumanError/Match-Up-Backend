caa3dc9812c39421f657e07de02c67a430ef932c
JunHyuk Yu <ericyoo0107@gmail.com>
2023-09-26 13:25:46 +0900

    feat: from 매서드 추가

--- src/main/java/com/example/matchup/matchupbackend/dto/response/teamuser/RecruitInfoResponse.java
+++ src/main/java/com/example/matchup/matchupbackend/dto/response/teamuser/RecruitInfoResponse.java
@@ -1,12 +1,16 @@
 package com.example.matchup.matchupbackend.dto.response.teamuser;
 
 import com.example.matchup.matchupbackend.dto.response.userposition.UserPositionDetailResponse;
+import com.example.matchup.matchupbackend.entity.TeamRecruit;
+import com.example.matchup.matchupbackend.entity.UserPosition;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class RecruitInfoResponse {
     // 유저 정보
-    private String userId;
+    private Long userId;
     private String thumbnailUrl;
     private String userName;
     private UserPositionDetailResponse userPosition;
@@ -15,4 +19,14 @@ public class RecruitInfoResponse {
     private String applyRole;
     private String content;
 
+    public static RecruitInfoResponse from(TeamRecruit recruit, UserPosition userPosition) {
+        return RecruitInfoResponse.builder()
+                .userId(recruit.getUser().getId())
+                .thumbnailUrl(recruit.getUser().getPictureUrl())
+                .userName(recruit.getUser().getName())
+                .userPosition(UserPositionDetailResponse.fromEntity(userPosition))
+                .applyRole(recruit.getRole())
+                .content(recruit.getContent())
+                .build();
+    }
 }
--- src/main/java/com/example/matchup/matchupbackend/dto/response/userposition/UserPositionDetailResponse.java
+++ src/main/java/com/example/matchup/matchupbackend/dto/response/userposition/UserPositionDetailResponse.java
@@ -1,5 +1,6 @@
 package com.example.matchup.matchupbackend.dto.response.userposition;
 
+import com.example.matchup.matchupbackend.entity.UserPosition;
 import lombok.Builder;
 import lombok.Data;
 
@@ -9,4 +10,11 @@ public class UserPositionDetailResponse {
 
     private String positionName;
     private Integer positionLevel;
+
+    public static UserPositionDetailResponse fromEntity(UserPosition userPosition) {
+        return UserPositionDetailResponse.builder()
+                .positionName(userPosition.getPositionName())
+                .positionLevel(userPosition.getPositionLevel())
+                .build();
+    }
 }
