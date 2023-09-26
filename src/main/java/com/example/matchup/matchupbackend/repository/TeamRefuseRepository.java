fe0dc714f84ed125b02debc5124ebabd15e0445e
JunHyuk Yu <ericyoo0107@gmail.com>
2023-09-27 01:47:54 +0900

    feat: 거절 정보 가져오는 DAO 추가

--- src/main/java/com/example/matchup/matchupbackend/repository/TeamRefuseRepository.java
+++ src/main/java/com/example/matchup/matchupbackend/repository/TeamRefuseRepository.java
@@ -2,6 +2,13 @@ package com.example.matchup.matchupbackend.repository;
 
 import com.example.matchup.matchupbackend.entity.TeamRefuse;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface TeamRefuseRepository extends JpaRepository<TeamRefuse, Long> {
+    @Query("select teamrefuse from TeamRefuse teamrefuse " +
+            "join fetch teamrefuse.refusedUser " +
+            "WHERE teamrefuse.id=:refuseId")
+    Optional<TeamRefuse> findRefuseInfoJoinUserById(Long refuseId);
 }
