dd4b9f55569ab97786892c59215c8ee7f156b35a
JunHyuk Yu <ericyoo0107@gmail.com>
2023-10-07 16:46:54 +0900

    fix: conflict 생기면서 변경사항 수정

--- src/main/java/com/example/matchup/matchupbackend/service/FileService.java
+++ src/main/java/com/example/matchup/matchupbackend/service/FileService.java
@@ -70,7 +70,7 @@ public class FileService {
      * base64를 디코딩 해서 File로 만드는 메서드
      */
     public File base64ToFile(String base64IMG, String storeFileName){
-        byte[] bytesIMG = Base64.getMimeDecoder().decode(base64IMG);
+        byte[] bytesIMG = java.util.Base64.getMimeDecoder().decode(base64IMG);
         try {
             File IMGFile = new File(storeFileName);
             FileOutputStream fos = new FileOutputStream(IMGFile);
