a87ec00db41c8b788a7ba74ae7acaaff22ae4fd1
jujemu <jujemu30@gmail.com>
2023-10-08 20:46:17 +0900

    Test: 응답하는 시간대의 포맷을 확인

--- src/main/java/com/example/matchup/matchupbackend/MatchUpBackendApplication.java
+++ src/main/java/com/example/matchup/matchupbackend/MatchUpBackendApplication.java
@@ -9,10 +9,6 @@ import java.util.TimeZone;
 @SpringBootApplication()
 public class MatchUpBackendApplication {
 
-    @PostConstruct
-    public void started() {
-        TimeZone.setDefault(TimeZone.getTimeZone("KST"));
-    }
     public static void main(String[] args) {
         SpringApplication.run(MatchUpBackendApplication.class, args);
     }
