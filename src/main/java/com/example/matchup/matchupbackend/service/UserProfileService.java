3620e68afd77d7950dc6e680a019b1bb859f3cc3
JunHyuk Yu <ericyoo0107@gmail.com>
2023-10-09 21:47:19 +0900

    fix: 에레메세지 수정

--- src/main/java/com/example/matchup/matchupbackend/service/UserProfileService.java
+++ src/main/java/com/example/matchup/matchupbackend/service/UserProfileService.java
@@ -24,7 +24,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.multipart.MultipartFile;
 import org.webjars.NotFoundException;
 
 import java.time.ZoneId;
