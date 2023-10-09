94308848b8732a5c1172c8eadf13c68a38b85ff1
JunHyuk Yu <ericyoo0107@gmail.com>
2023-10-09 22:15:17 +0900

    refactor: 필요 없는 메서드 삭제

--- src/main/java/com/example/matchup/matchupbackend/dto/request/team/TeamCreateRequest.java
+++ src/main/java/com/example/matchup/matchupbackend/dto/request/team/TeamCreateRequest.java
@@ -37,16 +37,6 @@ public class TeamCreateRequest {
     @NotEmpty(message = "모집 팀원은 필수 입력 값입니다.")
     private List<Member> memberList;
 
-    //계층구조가 있는 데이터는 굳이 클래스로 다 안나누고 1. 띄어쓰기 2. List로 보내고 받을수 있다(프론트와 상의)
-    public List<String> returnTagList() //사용자의 태그들을 모아서 중복을 제거함
-    {
-        List<String> userStacks = new ArrayList<>();
-        for (Member member : memberList) {
-            userStacks.addAll(member.getStacks());
-        }
-        return userStacks.stream().distinct().collect(Collectors.toList());
-    }
-
     public List<String> returnTagListByRole(String role) //사용자의 태그들을 모아서 중복을 제거함
     {
         List<String> userStacks = new ArrayList<>();
