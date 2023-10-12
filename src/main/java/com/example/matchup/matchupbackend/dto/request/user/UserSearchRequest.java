8bb36ee1aca036abcb2e4229774191badbffe62a
JunHyuk Yu <ericyoo0107@gmail.com>
2023-10-12 16:07:44 +0900

    fix: 유저 상세 검색 수정

--- src/main/java/com/example/matchup/matchupbackend/dto/request/user/UserSearchRequest.java
+++ src/main/java/com/example/matchup/matchupbackend/dto/request/user/UserSearchRequest.java
@@ -1,6 +1,7 @@
 package com.example.matchup.matchupbackend.dto.request.user;
 
 import com.example.matchup.matchupbackend.entity.MeetingType;
+import com.example.matchup.matchupbackend.global.RoleType;
 import lombok.Data;
 
 @Data
@@ -8,7 +9,7 @@ public class UserSearchRequest {
     private String search; // 닉네임 검색
     private MeetingType meetingType;
     private String place; //사는곳, 만남 위치
-    private String position; // 하고 싶은 포지션
+    private RoleType position; // 하고 싶은 포지션
     private String orderBy = "default"; //정렬 기준
     private int page;
     private int size;
