6471f08fa45691cfd3976bb6782bab0fd6e02950
JunHyuk Yu <ericyoo0107@gmail.com>
2023-10-22 15:05:51 +0900

    feat: 메세지 송수신중에 회원가입 한 유저인지 확인

---
+++ src/main/java/com/example/matchup/matchupbackend/chat/StompHandler.java
@@ -0,0 +1,37 @@
+package com.example.matchup.matchupbackend.chat;
+
+import com.example.matchup.matchupbackend.error.exception.AuthorizeException;
+import com.example.matchup.matchupbackend.global.config.jwt.TokenProvider;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class StompHandler implements ChannelInterceptor {
+    private final TokenProvider tokenProvider;
+
+    /**
+     * 메세지가 전송되기 전에 로그인 한 사용자인지 확인
+     */
+    @Override
+    public Message<?> preSend(Message<?> message, MessageChannel channel) {
+        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message); // 메세지 헤더에 접근하기 위한 객체
+        Optional<String> authorization = Optional.ofNullable(accessor.getFirstNativeHeader("Authorization"));
+        Long userId = tokenProvider.getUserId(authorization.orElseThrow(() -> new AuthorizeException("인증 토큰이 없습니다.")), "preSend");
+        log.info("userId: " + userId + " 가 문자를 전송하였습니다.");
+        return message;
+    }
+
+    @Override
+    public void postSend(Message<?> message, MessageChannel channel, boolean sent) {
+        log.info("postSend");
+    }
+}
