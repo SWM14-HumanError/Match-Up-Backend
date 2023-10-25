1ae5ce96be1799a8b4bc9abeaf69dfa87f69f367
JunHyuk Yu <ericyoo0107@gmail.com>
2023-10-26 01:08:08 +0900

    feat: dynamoDB는 localDateTime 저장 못해서 String으로 컨버터 생성

--- src/main/java/com/example/matchup/matchupbackend/config/DynamoDBConfig.java
+++ src/main/java/com/example/matchup/matchupbackend/config/DynamoDBConfig.java
@@ -1,5 +1,6 @@
 package com.example.matchup.matchupbackend.config;
 
+import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
@@ -7,11 +8,14 @@ import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTypeConverter;
 import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+
+import java.time.LocalDateTime;
 
 @Configuration
 @EnableDynamoDBRepositories(basePackages = "com.example.matchup.matchupbackend.dynamodb")
@@ -24,9 +28,26 @@ public class DynamoDBConfig {
     private String awsRegion;
 
 
-    @Bean
     public AWSCredentialsProvider amazonAWSCredentialsProvider() {
-        return new AWSStaticCredentialsProvider(new BasicAWSCredentials(awsAccessKey, awsSecretKey));
+        return new AWSStaticCredentialsProvider(amazonAWSCredentials());
+    }
+
+    @Bean
+    public AWSCredentials amazonAWSCredentials() {
+        return new BasicAWSCredentials(awsAccessKey, awsSecretKey);
+    }
+
+    @Bean
+    @Primary
+    public DynamoDBMapperConfig dynamoDBMapperConfig() {
+        return DynamoDBMapperConfig.DEFAULT;
+    }
+
+
+    @Bean
+    @Primary
+    public DynamoDBMapper dynamoDBMapper(AmazonDynamoDB amazonDynamoDB, DynamoDBMapperConfig config) {
+        return new DynamoDBMapper(amazonDynamoDB, config);
     }
 
     @Bean
@@ -34,16 +55,19 @@ public class DynamoDBConfig {
         return AmazonDynamoDBClientBuilder.standard().withCredentials(amazonAWSCredentialsProvider())
                 .withRegion(awsRegion).build();
     }
-    @Primary
-    @Bean
-    public DynamoDBMapper dynamoDBMapper(){
-        DynamoDBMapperConfig mapperConfig = DynamoDBMapperConfig.builder()
-                .withSaveBehavior(DynamoDBMapperConfig.SaveBehavior.CLOBBER)
-                .withConsistentReads(DynamoDBMapperConfig.ConsistentReads.CONSISTENT)
-                .withTableNameOverride(null)
-                .withPaginationLoadingStrategy(DynamoDBMapperConfig.PaginationLoadingStrategy.EAGER_LOADING)
-                .build();
 
-        return new DynamoDBMapper(amazonDynamoDB(), mapperConfig);
+    static public class LocalDateTimeConverter implements DynamoDBTypeConverter<String, LocalDateTime> {
+
+        @Override
+        public String convert( final LocalDateTime time ) {
+
+            return time.toString();
+        }
+
+        @Override
+        public LocalDateTime unconvert( final String stringValue ) {
+
+            return LocalDateTime.parse(stringValue);
+        }
     }
 }
