5435ea1dbd3f8b4d46c9bbd812b6096d9307c0a4
jujemu <jujemu30@gmail.com>
2023-10-25 19:10:51 +0900

    Merge: develop

--- src/main/java/com/example/matchup/matchupbackend/MatchUpBackendApplication.java
+++ src/main/java/com/example/matchup/matchupbackend/MatchUpBackendApplication.java
@@ -8,33 +8,38 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
+
+import java.util.Arrays;
 
 @RequiredArgsConstructor
 @SpringBootApplication()
 public class MatchUpBackendApplication {
 
+    private final Environment environment;
     private final UserRepository userRepository;
 
     @Profile("local")
     @PostConstruct
     public void createDummyUser() {
-        User user = User.builder()
-                .nickname("test")
-                .email("test@test.com")
-                .isMentor(true)
-                .role(Role.USER)
-                .build();
-        userRepository.save(user);
-
-        for (int i = 0; i < 5; i++) {
-            userRepository.save(User.builder()
-                    .nickname("test" + i)
-                    .email("test%d@test.com".formatted(i))
+        if(Arrays.asList(environment.getActiveProfiles()).contains("local")) {
+            User user = User.builder()
+                    .nickname("test")
+                    .email("test@test.com")
                     .isMentor(true)
                     .role(Role.USER)
-                    .build());
-        }
+                    .build();
+            userRepository.save(user);
 
+            for (int i = 0; i < 5; i++) {
+                userRepository.save(User.builder()
+                        .nickname("test" + i)
+                        .email("test%d@test.com".formatted(i))
+                        .isMentor(true)
+                        .role(Role.USER)
+                        .build());
+            }
+        }
     }
     public static void main(String[] args) {
         SpringApplication.run(MatchUpBackendApplication.class, args);
