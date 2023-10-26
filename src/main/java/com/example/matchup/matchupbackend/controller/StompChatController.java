09dc17cc426ae3a1c251ba00b36e0b36eede244f
JunHyuk Yu <ericyoo0107@gmail.com>
2023-10-26 17:35:22 +0900

    feat: 채팅 보내는 API 개발

--- src/main/java/com/example/matchup/matchupbackend/controller/StompChatController.java
+++ src/main/java/com/example/matchup/matchupbackend/controller/StompChatController.java
@@ -1,6 +1,7 @@
 package com.example.matchup.matchupbackend.controller;
 
 import com.example.matchup.matchupbackend.dto.request.chat.ChatMessageRequest;
+import com.example.matchup.matchupbackend.service.ChatService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
@@ -14,13 +15,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class StompChatController {
 
+    private final ChatService chatService;
+
     /**
      * 1대1 채팅 보내기
      */
     @MessageMapping("/chat/{roomId}") //"pub/chat/{roomId}"
     @SendToUser("/sub-queue/chat/{roomId}")
     public String sendMessage(@DestinationVariable Long roomId, ChatMessageRequest message) {
-        log.info(message.toString()); //todo 서비스단에서 채팅 저장하고 채팅 저장된 객체를 리턴
+        chatService.saveChatMessage(message);
         return message.getMessage();
     }
 
@@ -30,17 +33,8 @@ public class StompChatController {
     @MessageMapping("/chats/{roomId}") //"pub/chats/{roomId}"
     @SendTo("/sub-topic/chat/{roomId}")
     public String sendMessages(@DestinationVariable Long roomId, ChatMessageRequest message) {
-        log.info(message.toString());
+        chatService.saveChatMessage(message);
         return message.getMessage();
     }
 
-
-
-
-    @MessageMapping("/chat/test/{roomId}") //"queue/chat/pub/{roomId}", "topic/chat/pub/{roomId}"
-    @SendToUser("/queue/chat/test/{roomId}")
-    public String sendMessage(@DestinationVariable Long roomId, String message) {
-        log.info(message);
-        return message;
-    }
 }
