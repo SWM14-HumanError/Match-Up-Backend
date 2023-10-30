46d42dc0ac828b285f4692d08bdd144394e9ff62
JunHyuk Yu <ericyoo0107@gmail.com>
2023-10-30 17:48:08 +0900

    refactor: 필요없는 요소 삭제

--- src/main/java/com/example/matchup/matchupbackend/MatchUpBackendApplication.java
+++ src/main/java/com/example/matchup/matchupbackend/MatchUpBackendApplication.java
@@ -1,12 +1,10 @@
 package com.example.matchup.matchupbackend;
 
-import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.TimeZone;
 
-@SpringBootApplication()
+@SpringBootApplication
 public class MatchUpBackendApplication {
 
     public static void main(String[] args) {
