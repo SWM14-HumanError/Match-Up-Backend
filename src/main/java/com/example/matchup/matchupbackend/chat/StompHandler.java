c7cf874ee60edb36d3338011b8ecacb2573cf775
JunHyuk Yu <ericyoo0107@gmail.com>
2023-11-02 16:38:12 +0900

    test: 인증부분 주석처리

--- src/main/java/com/example/matchup/matchupbackend/chat/StompHandler.java
+++ src/main/java/com/example/matchup/matchupbackend/chat/StompHandler.java
@@ -9,8 +9,6 @@ import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-
 @Slf4j
 @Component
 @RequiredArgsConstructor
@@ -23,9 +21,9 @@ public class StompHandler implements ChannelInterceptor {
     @Override
     public Message<?> preSend(Message<?> message, MessageChannel channel) {
         StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message); // 메세지 헤더에 접근하기 위한 객체
-        List<String> authorization = accessor.getNativeHeader("Authorization");
-        Long userId = tokenProvider.getUserId(authorization.get(0), "preSend");
-        log.info("userId: " + userId + " 가 문자를 전송하였습니다.");
+        //String authorization = String.valueOf(accessor.getNativeHeader("Authorization"));
+        //Long userId = tokenProvider.getUserId(authorization, "preSend");
+        //log.info("userId: " + userId + " 가 문자를 전송하였습니다.");
         return message;
     }
 
