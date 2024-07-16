0da367e17b0205bc8d60b09f1241b8edad930fed
JunHyuk Yu <ericyoo0107@gmail.com>
2024-07-16 23:59:30 +0900

    feat: 몽고 DB용 채팅 컨트롤러 제작

---
+++ src/main/java/com/example/matchup/matchupbackend/config/MongoDBConfig.java
@@ -0,0 +1,36 @@
+package com.example.matchup.matchupbackend.config;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import lombok.RequiredArgsConstructor;
+import org.bson.codecs.configuration.CodecRegistry;
+import org.bson.codecs.pojo.PojoCodecProvider;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
+import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableMongoAuditing
+@EnableMongoRepositories(basePackages = "com.example.matchup.matchupbackend.mongodb")
+public class MongoDBConfig {
+    @Value("${spring.data.mongodb.uri}")
+    private String connectionString;
+
+    @Bean
+    public MongoClient mongoClient() {
+        CodecRegistry pojoCodecRegistry = fromProviders(PojoCodecProvider.builder().automatic(true).build());
+        CodecRegistry codecRegistry = fromRegistries(MongoClientSettings.getDefaultCodecRegistry(), pojoCodecRegistry);
+        return MongoClients.create(MongoClientSettings.builder()
+                .applyConnectionString(new ConnectionString(connectionString))
+                .codecRegistry(codecRegistry)
+                .build());
+    }
+}
