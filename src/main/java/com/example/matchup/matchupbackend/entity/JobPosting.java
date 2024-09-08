2cacf45b688033953e7d6d490cd789d724ce33e1
JunHyuk Yu <ericyoo0107@gmail.com>
2024-09-09 00:37:30 +0900

    feat: Entity에 메타데이터 추가 및 Enum 값 영어로 변경

--- src/main/java/com/example/matchup/matchupbackend/entity/JobPosting.java
+++ src/main/java/com/example/matchup/matchupbackend/entity/JobPosting.java
@@ -13,7 +13,7 @@ import java.time.LocalDateTime;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class JobPosting {
+public class JobPosting extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "job_posting_id")
--- src/main/java/com/example/matchup/matchupbackend/entity/JobType.java
+++ src/main/java/com/example/matchup/matchupbackend/entity/JobType.java
@@ -6,5 +6,5 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum JobType {
-    부트캠프, 인턴, 신입, 주니어, 미들, 시니어, 기타
+    BOOTCAMP, INTERN, NEWBIE, JUNIOR, MIDDLE, SENIOR, ETC
 }
